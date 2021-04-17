7f3c5d256d5570b5478fcff686bda696710303a0
Diego Zubaray <diegozubaray@gmail.com>
2021-04-17 13:16:57 -0300

    deploy en netlify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dados';
+
+ dadoIzquiera='../assets/img/dice1.png'
+ dadoDerecha='../assets/img/dice4.png'
+ numero1:number = 4;
+ numero2:number = 6;
+
+ tirarDados():void{
+
+   this.numero1= Math.round(Math.random() * 5) + 1 ;
+   this.numero2= Math.round(Math.random() * 5) + 1;
+
+   this.dadoIzquiera = '../assets/img/dice' + this.numero1 + '.png';
+   this.dadoDerecha = '../assets/img/dice' + this.numero2 + '.png';
+ }
+
 }
